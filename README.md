f47187b872dca59ed5473f93d962831b056c1db5
David Rubert <david.rubert@gmail.com>
2016-02-03 16:40:45 +0100

    run.sh script working

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # red
+
+## Install instructions
+**Requeriments**: python2, docker, docker-compose, bower
+
+You have available an install-helper script which will download, build and run the containers
+
+```
+$ ./run.sh
+```
--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 pushd `dirname $0` > /dev/null
 
-docker rm -f red
 bower install
-docker build -t uji/red .
-docker run -d -p 5000:5000 -v /opt/devel/workspaces/uji/red/data:/data --name red uji/red
+docker-compose build
+docker-compose --x-networking up
 
 popd > /dev/null
